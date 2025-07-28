a64870c40ab2aae287cc147ef913f6732513bf9d
= <=>
2025-07-28 17:56:30 -0300

    prueba

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,8 @@ async function bootstrap() {
     app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.port);
 
+  console.log('hola mundo - primer cambio')
+
   logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
